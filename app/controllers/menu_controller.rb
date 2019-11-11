facfff3b1435aea2c038ca05b79d1d4188763534
Ubuntu <ubuntu@ip-172-31-84-106.ec2.internal>
2019-11-11 13:59:11 +0000

    DB

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -3,4 +3,12 @@ class MenuController < ApplicationController
     def teste
     end
     
+    def root
+    end
+    
+    def numero()
+        @var = params[:x]
+        @var2 = params[:y]
+    end
+    
 end
