1b04e323c259d4d3a0b5fd7c2a9058cca0b131af
Ubuntu <ubuntu@ip-172-31-84-106.ec2.internal>
2019-11-11 23:16:03 +0000

    Imagens

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,14 +1,14 @@
 class MenuController < ApplicationController
     
-    def teste
-    end
+#    def teste
+#    end
     
     def root
     end
     
-    def numero()
-        @var = params[:x]
-        @var2 = params[:y]
-    end
+#    def numero()
+#        @var = params[:x]
+#        @var2 = params[:y]
+#    end
     
 end
